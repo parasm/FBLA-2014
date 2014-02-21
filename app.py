5edd85a0e2666f01f182066d998ad4346388d996
Paras Modi <parasm346@gmail.com>
2014-02-21 14:52:55 -0500

    url handlers

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return render_template("index.html")
+@app.route('/buy')
+def buy():
+	return render_template("buy.html")
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 8000))
 	app.run(host='0.0.0.0', port=port,debug=True)
